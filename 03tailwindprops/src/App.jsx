eb14636daedd52fd6a4fec8958ef3337b6d35665
Radha <radhasharma2786@gmail.com>
2023-12-06 23:50:14 +0530

    03Tailwind~props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const arr =[1,2,3];
+  const obj ={
+    name:"radha",
+    age:19 
+  }
+
+  return (
+    <>
+  <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Props</h1>
+<Card username="Yash" myarray= {arr} myobj={obj} button="show" />
+<Card username="Radha Sharma" button="visit me"/>
+    </>
+  )
+}
+
+export default App
